c517e7a9b7c2cfc13aea2ca5b6b488adeb16c2ba
Vladimir Kovner <wolves3d@gmail.com>
2015-07-10 01:13:35 +0300

    line / plane collision

--- _Shared/Math/plane.h
+++ _Shared/Math/plane.h
@@ -9,9 +9,10 @@ class CPlane
 	{
 	};
 
-	inline CPlane( const vec3 & vNrml, const float fOff ) :
-		vNormal( vNrml	),
-		fOffset( fOff	)
+	inline CPlane( const vec3 & vNrml, const float fOff )
+		: vNormal(vNrml)
+		, fOffset(fOff)
+		, vOrigin(vec3::vNull)
 	{
 	}
 
@@ -25,6 +26,27 @@ class CPlane
 		fOffset		*= fInvMag;
 	}
 
+	bool LineCollision(vec3 * outHitPoint, const Line & line)
+	{
+		// вычисляем расстояния между концами отрезка и плоскостью треугольника.
+		float r1 = Dot(vNormal, line.vStart - vOrigin);
+		float r2 = Dot(vNormal, line.vEnd - vOrigin);
+
+		// если оба конца отрезка лежат по одну сторону от плоскости, то отрезок
+		// не пересекает треугольник.
+		if (f1_sgn(r1) == f1_sgn(r2))
+			return false;
+
+		if (0 != outHitPoint)
+		{
+			// вычисляем точку пересечения отрезка с плоскостью треугольника.
+			(*outHitPoint) = (line.vStart + ((line.vEnd - line.vStart) * (-r1 / (r2 - r1))));
+		}
+
+		return true;
+	}
+
 	vec3	vNormal;
+	vec3	vOrigin;
 	float	fOffset;
 };
